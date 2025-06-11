e338f8214fdec6108e1ce00a33b9b6ceb05ab392
Muhammad Ahmad <ahmadwebcrafts@gmail.com>
2025-06-11 19:07:29 +0500

    resolve error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,6 @@
 // app/layout.tsx or app/RootLayout.tsx
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import localfont from "next/font/local"
 import "./globals.css";
-
-const Roca = localfont(
-  {
-    src: [
-      { path: '../public/fonts/rocabold.ttf', weight: "700" }, 
-    ],
-    variable: '--font-roca',
-  }
-);
-
-const RocaLight = localfont(
-  {
-    src: [
-      { path: '../public/fonts/rocathin.ttf', weight: "800" }, 
-    ],
-    variable: '--font-rocathin',
-  }
-);
-
-// Load Inter font with all weights
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
-});
 
 
 
@@ -44,7 +17,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
       <html lang="en">
-        <body className={`${inter.variable} ${Roca.variable} ${RocaLight.variable} antialiased bg-white`}>
+        <body className={`antialiased bg-white`}>
           
           {children}
         </body>
